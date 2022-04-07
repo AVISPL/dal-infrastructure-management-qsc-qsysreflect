2d305b8889efefa73e655466fa511ff54e2d3a3c
HieuLM-AVISPL <hm15015@avispl.com>
2022-04-07 17:04:53 +0700

    SYAL-872 Implement monitoring for the devices other than Q-SYS Core and filter And UT for it

--- src/main/java/com/avispl/symphony/dal/infrastructure/management/qsc/qsysreflect/utils/QSysReflectConstant.java
+++ src/main/java/com/avispl/symphony/dal/infrastructure/management/qsc/qsysreflect/utils/QSysReflectConstant.java
@@ -6,9 +6,9 @@ package com.avispl.symphony.dal.infrastructure.management.qsc.qsysreflect.utils;
 /**
  * QSys Reflect Constant class
  *
- * @author Duy Nguyen
- * @version 1.0.0
- * @since 1.0.0
+ * @author Duy Nguyen, Harry
+ * @version 2.0.0
+ * @since 2.0.0
  */
 public class QSysReflectConstant {
 
@@ -17,11 +17,16 @@ public class QSysReflectConstant {
 	public static final String MESSAGE = "message";
 	public static final String COMMA = ",";
 	public static final String DEVICE_UPTIME = "deviceUptime";
+	public static final String START_AT = "startedAt";
 	public static final String DEVICE_STATUS_MESSAGE = "deviceStatusMessage";
 	public static final String DOUBLE_QUOTES = "";
 	public static final String QSYS_URL_SYSTEMS = "/systems";
 	public static final String QSYS_URL_CORES = "/cores";
+	public static final String QSYS_URL_ITEMS = "/items";
 	public static final String QSYS_BASE_URL = "/api/public/v0";
 	public static final String NONE = "None";
 	public static final String RUNNING = "Running";
+	public static final String OK = "OK";
+	public static final String DEVICE_TYPE = "deviceType";
+	public static final String CORE = "Core";
 }
